ff197d7a25a516515fcf578c7b45ff49216870c3
Joy <90715022+Dogifu@users.noreply.github.com>
2023-11-30 02:41:56 +0300

    Add files via upload
    block_7

--- main.cpp
+++ main.cpp
@@ -1,281 +1,60 @@
 //
 //  main.cpp
-//  ФИЛЬТР БЛУМА
+//  блок
 //
 //  Created by user on 21.11.2023.
 //
 
-
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <sstream>
-#include <stdexcept>
+#include <algorithm>
 
-class BloomFilter
-{
-private:
-    std::vector<bool> bitArray;
-    unsigned long long size;
-    unsigned long long numHashFunctions;
-    bool isSet;
+int main() {
+    int n, p, b, b_max, cur_unix;
 
-    static const unsigned long long MERSENNE_PRIME = 2147483647;
-
-    static unsigned long long getNthPrime(unsigned long long n)
-    {
-        if (n <= 0)
-            return 0;
-        if (n == 1)
-            return 2;
-
-        unsigned long long count = 1;
-        unsigned long long num = 3;
-
-        while (count < n)
-        {
-            bool isPrime = true;
-
-            for (unsigned long long i = 2; i <= static_cast<unsigned long long>(sqrt(num)); ++i)
-            {
-                if (num % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if (isPrime)
-            {
-                count++;
-            }
-
-            if (count < n)
-            {
-                num += 2;
-            }
-        }
-
-        return num;
+    try {
+        std::cin >> n >> p >> b >> b_max >> cur_unix;
+    } catch (const std::exception& e) {
+        return 1;
     }
 
-    unsigned long long hashFunction(unsigned long long i, unsigned long long key) const
-    {
-        unsigned long long prime = getNthPrime(i + 1);
-        return static_cast<unsigned long long>((((i + 1) * key + prime) % MERSENNE_PRIME) % size);
+    std::vector<int> tries;
+
+    int failure;
+    while (std::cin >> failure) {
+        if (failure >= cur_unix - 2 * b_max) {
+            tries.push_back(failure);
+        }
     }
 
-public:
-    BloomFilter() : size(0), numHashFunctions(0), isSet(false) {}
+    std::sort(tries.begin(), tries.end());
 
-    bool set(float n, double P, float &outSize, float &outNumHashFunctions)
-    {
-        if (isSet || n == 0 || P >= 1 || P <= 0)
-        {
-            outSize = 0;
-            outNumHashFunctions = 0;
-            return false;
-        }
+    int b_min = b;
+    int b_cur = b_min;
+    bool blocked_before = false;
+    int block_start = 0;
+    int try_num = 0;
 
-        size = static_cast<double>(std::round(-n * log2(P) / log(2)));
-        numHashFunctions = static_cast<double>(std::round(-log2(P)));
-
-        if (size == 0 || numHashFunctions == 0)
-        {
-            outSize = 0;
-            outNumHashFunctions = 0;
-            return false;
-        }
-
-        bitArray = std::vector<bool>(size, false);
-        isSet = true;
-
-        outSize = size;
-        outNumHashFunctions = numHashFunctions;
-        return true;
-    }
-
-    bool add(long long key)
-    {
-        if (!isSet || key < 0)
-        {
-            throw std::runtime_error("Structure is not initialized or invalid input");
-        }
-
-        for (unsigned long long i = 0; i < numHashFunctions; ++i)
-        {
-            if (key < 0)
-            {
-                throw std::runtime_error("Invalid input: negative key");
-            }
-
-            unsigned long long index = hashFunction(i, key);
-            bitArray[index] = true;
-        }
-
-        return true;
-    }
-
-    bool search(long long key) const
-    {
-        if (!isSet || key < 0)
-        {
-            throw std::runtime_error("Structure is not initialized or invalid input");
-        }
-
-        for (unsigned long long i = 0; i < numHashFunctions; ++i)
-        {
-            if (key < 0)
-            {
-                throw std::runtime_error("Invalid input: negative key");
-            }
-
-            unsigned long long index = hashFunction(i, key);
-            if (!bitArray[index])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    std::string print() const
-    {
-        if (!isSet)
-        {
-            return "";
-        }
-
-        std::string result;
-        for (bool bit : bitArray)
-        {
-            result += (bit ? "1" : "0");
-        }
-        return result;
-    }
-};
-
-int main()
-{
-    BloomFilter bloomFilter;
-
-    float m, k;
-
-    std::string command;
-    while (std::getline(std::cin, command))
-    {
-        if (command.empty())
-        {
+    while (try_num + n <= tries.size()) {
+        if (tries[try_num + n - 1] - tries[try_num] >= p) {
+            try_num++;
             continue;
         }
 
-        if (command.front() == ' ' || command.back() == ' ')
-        {
-            std::cout << "error" << std::endl;
-            continue;
+        if (blocked_before) {
+            b_cur *= 2;
+            b_cur = std::min(b_max, b_cur);
         }
 
-        std::istringstream iss(command);
-        std::string cmd;
-        iss >> cmd;
+        try_num += n;
+        block_start = tries[try_num - 1];
+        blocked_before = true;
+    }
 
-        try
-        {
-            if (cmd == "set")
-            {
-                float n;
-                double P;
-                if (iss >> n >> P && iss.eof())
-                {
-                    if (bloomFilter.set(n, P, m, k))
-                    {
-                        std::cout << m << " " << k << std::endl;
-                    }
-                    else
-                    {
-                        std::cout << "error" << std::endl;
-                    }
-                }
-                else
-                {
-                    throw std::runtime_error("Invalid input");
-                }
-            }
-            else if (cmd == "add")
-            {
-                float key;
-                char nextChar;
-
-                if (iss >> key && iss.eof() && !iss.get(nextChar))
-                {
-                    if (command.find("-0") != std::string::npos)
-                    {
-                        std::cout << "error" << std::endl;
-                    }
-                    else
-                    {
-                        try
-                        {
-                            bloomFilter.add(key);
-                        }
-                        catch (const std::exception &e)
-                        {
-                            std::cout << "error" << std::endl;
-                        }
-                    }
-                }
-                else
-                {
-                    throw std::runtime_error("Invalid input");
-                }
-            }
-            else if (cmd == "search")
-            {
-                float key;
-                if (iss >> key && iss.eof())
-                {
-                    if (command.find("-0") != std::string::npos)
-                    {
-                        std::cout << "error" << std::endl;
-                    }
-                    else
-                    {
-                        try
-                        {
-                            bool searchResult = bloomFilter.search(key);
-                            std::cout << (searchResult ? "1" : "0") << std::endl;
-                        }
-                        catch (const std::exception &e)
-                        {
-                            std::cout << "error" << std::endl;
-                        }
-                    }
-                }
-                else
-                {
-                    throw std::runtime_error("Invalid input");
-                }
-            }
-            else if (cmd == "print" && iss.eof())
-            {
-                std::string result = bloomFilter.print();
-                if (result == "")
-                {
-                    std::cout << "error" << std::endl;
-                }
-                else
-                {
-                    std::cout << result << std::endl;
-                }
-            }
-            else
-            {
-                throw std::runtime_error("Invalid command");
-            }
-        }
-        catch (const std::exception &e)
-        {
-            std::cout << "error" << std::endl;
-        }
+    if (block_start + b_cur < cur_unix || !blocked_before) {
+        std::cout << "ok" << std::endl;
+    } else {
+        std::cout << block_start + b_cur << std::endl;
     }
 
     return 0;
